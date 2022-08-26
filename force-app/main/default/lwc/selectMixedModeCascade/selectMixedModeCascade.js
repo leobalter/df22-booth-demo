a58231088e02a887c9a4c6aa403b55f9f9738858
Leo Balter <leonardo.balter@gmail.com>
2022-08-25 23:45:32 -0700

    simplify

--- force-app/main/default/lwc/selectMixedModeCascade/selectMixedModeCascade.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class SelectMixedModeCascade extends LightningElement {
-    handleClick() {
-        this.template.querySelectorAll('.animal').forEach(elem => {
-            elem.classList.toggle('highlight');
-        });
-    }
-}
